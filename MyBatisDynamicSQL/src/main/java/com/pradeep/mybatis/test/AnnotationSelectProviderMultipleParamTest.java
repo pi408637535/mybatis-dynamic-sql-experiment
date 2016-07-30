650e0d26da7e9f953c1ff1dc17bb36f15a2862db
Pradeep B <pradeepbhat4591@gmail.com>
2016-07-30 14:38:32 +0530

    Add files via upload

---
+++ MyBatisDynamicSQL/src/main/java/com/pradeep/mybatis/test/AnnotationSelectProviderMultipleParamTest.java
@@ -0,0 +1,37 @@
+package com.pradeep.mybatis.test;
+
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import com.pradeep.mybatis.mapper.StudentMapper;
+import com.pradeep.mybatis.model.Student;
+
+public class AnnotationSelectProviderMultipleParamTest {
+
+	public static void main(String args[]) throws IOException {
+
+		Reader reader = Resources.getResourceAsReader("SqlMapConfig.xml");
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder()
+				.build(reader);
+		SqlSession session = sqlSessionFactory.openSession();
+		session.getConfiguration().addMapper(StudentMapper.class);
+		StudentMapper mapper = session.getMapper(StudentMapper.class);
+
+		Student student = mapper.findStudentByNameAndEmail("Giri", "giri@goal.com");
+		System.out.println(student.getBranch());
+		System.out.println(student.getEmail());
+		System.out.println(student.getId());
+		System.out.println(student.getName());
+		System.out.println(student.getPercentage());
+		System.out.println(student.getPhone());
+		session.commit();
+		session.close();
+
+	}
+
+}
